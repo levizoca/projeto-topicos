c3b941f90caec8bacec3f3642e95d3975b3276c3
Levi Motta <levi.motta@hotmail.com>
2022-08-30 13:36:01 +0000

    mapemaento classe autorização

---
+++ src/main/java/br/gov/sp/fatec/projetotopicos/entity/Autorizacao.java
@@ -0,0 +1,53 @@
+package br.gov.sp.fatec.projetotopicos.entity;
+
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+@Entity
+@Table(name = "aut_autorizacao")
+public class Autorizacao {
+
+    public Autorizacao(String nome){
+        this.nome = nome;
+    }
+
+    public Autorizacao() {}
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "aut_id")
+    private Long id;
+
+    @Column(name = "aut_nome")
+    private String nome;
+
+    @JsonIgnore
+    @ManyToMany(fetch= FetchType.LAZY, mappedBy = "autorizacoes")
+    private Set<Usuario> usuarios;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
