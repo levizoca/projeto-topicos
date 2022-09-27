e284bfb708a65af1edff84d4e7758737de722f49
Levi Motta <levi.motta@hotmail.com>
2022-09-27 02:16:00 +0000

    added security and login

--- src/main/java/br/gov/sp/fatec/projetotopicos/service/SegurancaService.java
+++ src/main/java/br/gov/sp/fatec/projetotopicos/service/SegurancaService.java
@@ -1,16 +1,20 @@
 package br.gov.sp.fatec.projetotopicos.service;
 
+import br.gov.sp.fatec.projetotopicos.entity.Usuario;
 import java.util.List;
 
-import br.gov.sp.fatec.projetotopicos.entity.Usuario;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
-public interface SegurancaService {
-
+public interface SegurancaService extends UserDetailsService {
+    
     public Usuario novoUsuario(Usuario usuario);
 
     public Usuario novoUsuario(String nome, String senha);
 
+    public Usuario novoUsuario(String nome, String senha, String autorizacao);
+
     public List<Usuario> todosUsuarios();
 
     public Usuario buscarPorId(Long id);
+
 }
--- src/main/java/br/gov/sp/fatec/projetotopicos/service/SegurancaServiceImpl.java
+++ src/main/java/br/gov/sp/fatec/projetotopicos/service/SegurancaServiceImpl.java
@@ -1,51 +1,90 @@
 package br.gov.sp.fatec.projetotopicos.service;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import br.gov.sp.fatec.projetotopicos.entity.Autorizacao;
 import br.gov.sp.fatec.projetotopicos.entity.Usuario;
 import br.gov.sp.fatec.projetotopicos.repository.UsuarioRepository;
+import br.gov.sp.fatec.projetotopicos.repository.AutorizacaoRepository;
 
 @Service
 public class SegurancaServiceImpl implements SegurancaService {
 
     @Autowired
-    private UsuarioRepository usuarioRepository;
+    private UsuarioRepository usuarioRepo;
+
+    @Autowired
+    private AutorizacaoRepository autorizacaoRepo;
 
     @Override
+    @PreAuthorize("hasRole('ADMIN')")
     public Usuario novoUsuario(Usuario usuario) {
-        return usuarioRepository.save(usuario);
+        return usuarioRepo.save(usuario);
     }
 
     @Override
     public Usuario novoUsuario(String nome, String senha) {
-
         Usuario usuario = new Usuario();
         usuario.setNome(nome);
         usuario.setSenha(senha);
-
         return novoUsuario(usuario);
     }
 
     @Override
-    public List<Usuario> todosUsuarios() {
-        return usuarioRepository.findAll();
+    @Transactional
+    public Usuario novoUsuario(String nome, String senha, String autorizacao) {
+        Autorizacao aut = autorizacaoRepo.findByNome(autorizacao);
+        if(aut == null) {
+            aut = new Autorizacao();
+            aut.setNome(autorizacao);
+            autorizacaoRepo.save(aut);
+        }
+        Usuario usuario = new Usuario();
+        usuario.setNome(nome);
+        usuario.setSenha(senha);
+        usuario.setAutorizacoes(new HashSet<Autorizacao>());
+        usuario.getAutorizacoes().add(aut);
+        return novoUsuario(usuario);
     }
 
     @Override
+    @PreAuthorize("isAuthenticated()")
+    public List<Usuario> todosUsuarios() {
+        return usuarioRepo.findAll();
+    }
+
+    @Override
+    @PreAuthorize("isAuthenticated()")
     public Usuario buscarPorId(Long id) {
-        
-        Optional<Usuario> usuarioOptional = usuarioRepository.findById(id);
-        
-        if (usuarioOptional.isPresent()) {
+        Optional<Usuario> usuarioOptional = usuarioRepo.findById(id);
+        if(usuarioOptional.isPresent()) {
             return usuarioOptional.get();
         }
-
-        throw new RuntimeException("Usuario nao encontrado");
+        throw new RuntimeException("Usuario nao encontrado!");
     }
-    
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Usuario usuario = usuarioRepo.buscarPorNome(username);
+        if (usuario == null) {
+          throw new UsernameNotFoundException("Usuário " + username + " não encontrado!");
+        }
+        return User.builder().username(username).password(usuario.getSenha())
+            .authorities(usuario.getAutorizacoes().stream()
+                .map(Autorizacao::getNome).collect(Collectors.toList())
+                .toArray(new String[usuario.getAutorizacoes().size()]))
+            .build();
+    }
     
 }
